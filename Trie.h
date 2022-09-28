8cd8554318b06201f9d25ace8653eeb7a1a342b1
JacksonHagood <jagmachat@gmail.com>
2022-09-28 00:19:12 -0500

    getCandidates() start

--- Trie.h
+++ Trie.h
@@ -2,11 +2,20 @@
 # define _TRIE_H
 
 # include "Node.h"
+
+# include <vector>
 # include <string>
+# include <iostream>
 # include <fstream>
 
 /* Trie Class Design:
-    Trie can be thought of having 26 roots, one for each alphabetic character.
+    Trie can be thought of having 26 roots, one for each alphabetic character. Trie is constructed
+    using a dictionary file. This constructor creates nodes as needed and sets priority (dictionary
+    must be in priority order).
+
+    To find candidates, Trie is first traversed to find the final known node (or see if no such
+    word exists). Then, candidates are found using the priority arrays present in each of the
+    nodes.
 */
 
 class Trie {
@@ -14,14 +23,31 @@ class Trie {
         Node* root[26];
         
     public:
+        // TODO: copy constructor
+        Trie(const Trie& other) {
+            // TODO: implement
+        }
+
+        // copy assignment operator
+        Trie& operator=(const Trie& other) {
+            // TODO: implement
+
+            return *this;
+        }
+
+        // destructor
+        ~Trie() {
+            // TODO: traverse tree and delete nodes
+        }
+
         // constructor with file name
         Trie(const std::string fileName) {
-            // add 26 root nodes
+            // initialize 26 root nodes
             for (unsigned int i = 0; i < 26; i++) {
                 root[i] = new Node(i + 'a');
             }
 
-            // open dictionary
+            // open dictionary file
             std::ifstream input(fileName);
             std::string word;
 
@@ -29,40 +55,38 @@ class Trie {
             while (input >> word) {
                 Node* currNode = root[word.at(0) - 'a'];
 
+                // iterate through characters
                 for (unsigned int i = 1; i < word.size(); i++) {
                     if (currNode->getNode(word.at(i)) == nullptr) {
-                        // insert node
+                        // insert node if none exists
                         Node* newNode = new Node(word.at(i));
                         currNode->addNode(newNode);
                     }
 
-                    // iterate
                     currNode = currNode->getNode(word.at(i));
                 }
                 
                 currNode->setTerminal();
-
             }
         }
 
-        // TODO: copy constructor
-        Trie(const Trie& other) {
-
-        }
-
-        // TODO: copy assignment operator
-        Trie& operator=(const Trie& other) {
-            return *this;
-        }
-
-        // TODO: destructor
-        ~Trie() {
-
-        }
-
         // TODO: gets a candidate of priority i for the partial word
-        std::string getCandidate(std::string partial, unsigned int i) {
-            return "";
+        int getCandidates(std::string partial, std::string candidates[3]) {
+            Node* currNode = root[partial.at(0) - 'a'];
+
+            for (unsigned int i = 1; i < partial.size(); i++) {
+                // avoid dereferencing nullptr (no such word exists)
+                if (currNode->getNode(partial.at(i)) == nullptr) {
+                    return 0;
+                }
+
+                // iterate through nodes to get final character node
+                currNode = currNode->getNode(partial.at(i));
+            }
+
+            // TODO: use currNode to determine three auto-complete candidates using priority arrays in nodes
+
+            return 1;
         }
 };
 
